a1d8791f1533d162d5ea2af7c1a1bd9c490bfaaa
isGuoHao <guohaoprc@163.com>
2024-11-24 19:38:41 +0800

    - Issues: 修改GPIO Driver为Platform Driver，根据原设备类型，选用实际设备驱动 - Date: 11/24/24 19:38:41 - Author: wanguo

--- src/template/drivers/pdm_driver_template_platform.c
+++ src/template/drivers/pdm_driver_template_platform.c
@@ -1,23 +1,9 @@
-// SPDX-License-Identifier: GPL-2.0-only
-/*
- * LEDs driver for GPIOs
- *
- * Copyright (C) 2007 8D Technologies inc.
- * Raphael Assenat <raph@8d.com>
- * Copyright (C) 2008 Freescale Semiconductor, Inc.
- */
 #include <linux/container_of.h>
 #include <linux/device.h>
 #include <linux/err.h>
-#include <linux/gpio.h>
-#include <linux/gpio/consumer.h>
-#include <linux/leds.h>
 #include <linux/mod_devicetable.h>
 #include <linux/module.h>
-#include <linux/overflow.h>
-#include <linux/pinctrl/consumer.h>
 #include <linux/platform_device.h>
-#include <linux/property.h>
 #include <linux/slab.h>
 #include <linux/types.h>
 
@@ -25,7 +11,7 @@
 #include "pdm_template.h"
 
 
-static int pdm_template_gpio_probe(struct platform_device *pdev)
+static int pdm_template_platform_probe(struct platform_device *pdev)
 {
     struct pdm_device *pdmdev;
     struct pdm_template_device_priv *pstTemplateDevPriv;
@@ -38,7 +24,7 @@ static int pdm_template_gpio_probe(struct platform_device *pdev)
     }
 
     pdmdev->real_device = pdev;
-    strcpy(pdmdev->compatible, "pdm,template-gpio");
+    strcpy(pdmdev->compatible, "pdm,template-platform");
     ret = pdm_template_master_register_device(pdmdev);
     if (ret) {
         OSA_ERROR("Failed to add template device, ret=%d.\n", ret);
@@ -53,7 +39,7 @@ static int pdm_template_gpio_probe(struct platform_device *pdev)
     }
     pstTemplateDevPriv->ops = NULL;
 
-    OSA_INFO("Template GPIO Device Probed.\n");
+    OSA_INFO("Template PLATFORM Device Probed.\n");
     return 0;
 
 unregister_pdmdev:
@@ -66,7 +52,7 @@ free_pdmdev:
 
 }
 
-static int pdm_template_gpio_remove(struct platform_device *pdev)
+static int pdm_template_platform_remove(struct platform_device *pdev)
 {
     struct pdm_device *pdmdev = pdm_template_master_find_pdmdev(pdev);
     if (NULL == pdmdev) {
@@ -77,63 +63,63 @@ static int pdm_template_gpio_remove(struct platform_device *pdev)
     pdm_template_master_unregister_device(pdmdev);
     pdm_device_free(pdmdev);
 
-    OSA_INFO("Template GPIO Device Removed.\n");
+    OSA_INFO("Template PLATFORM Device Removed.\n");
     return 0;
 }
 
 /**
  * @ dts节点配置示例
- *  template-gpio-0 {
- *      compatible = "pdm,template-gpio";
+ *  template-platform-0 {
+ *      compatible = "pdm,template-platform";
  *      status = "okay";
  *  };
 */
-static const struct of_device_id of_gpio_leds_match[] = {
-	{ .compatible = "pdm,template-gpio", },
+static const struct of_device_id of_platform_leds_match[] = {
+	{ .compatible = "pdm,template-platform", },
 	{},
 };
 
-MODULE_DEVICE_TABLE(of, of_gpio_leds_match);
+MODULE_DEVICE_TABLE(of, of_platform_leds_match);
 
-static struct platform_driver pdm_template_gpio_driver = {
-	.probe		= pdm_template_gpio_probe,
-	.remove	= pdm_template_gpio_remove,
+static struct platform_driver pdm_template_platform_driver = {
+	.probe		= pdm_template_platform_probe,
+	.remove	= pdm_template_platform_remove,
 	.driver		= {
-		.name	= "pdm-template-gpio",
-		.of_match_table = of_gpio_leds_match,
+		.name	= "pdm-template-platform",
+		.of_match_table = of_platform_leds_match,
 	},
 };
 
 
 /**
- * @brief 初始化 GPIO 驱动
+ * @brief 初始化 PLATFORM 驱动
  *
- * 该函数用于初始化 GPIO 驱动，注册 GPIO 驱动到系统。
+ * 该函数用于初始化 PLATFORM 驱动，注册 PLATFORM 驱动到系统。
  *
  * @return 成功返回 0，失败返回负错误码
  */
-int pdm_template_gpio_driver_init(void) {
+int pdm_template_platform_driver_init(void) {
     int ret;
 
-    ret = platform_driver_register(&pdm_template_gpio_driver);
+    ret = platform_driver_register(&pdm_template_platform_driver);
     if (ret) {
-        OSA_ERROR("Failed to register Template GPIO Driver.\n");
+        OSA_ERROR("Failed to register Template PLATFORM Driver.\n");
         return ret;
     }
-    OSA_INFO("Template GPIO Driver Initialized.\n");
+    OSA_INFO("Template PLATFORM Driver Initialized.\n");
     return 0;
 }
 
 /**
- * @brief 退出 GPIO 驱动
+ * @brief 退出 PLATFORM 驱动
  *
- * 该函数用于退出 GPIO 驱动，注销 GPIO 驱动。
+ * 该函数用于退出 PLATFORM 驱动，注销 PLATFORM 驱动。
  */
-void pdm_template_gpio_driver_exit(void) {
-    platform_driver_unregister(&pdm_template_gpio_driver);
-    OSA_INFO("Template GPIO Driver Exited.\n");
+void pdm_template_platform_driver_exit(void) {
+    platform_driver_unregister(&pdm_template_platform_driver);
+    OSA_INFO("Template PLATFORM Driver Exited.\n");
 }
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("<guohaoprc@163.com>");
-MODULE_DESCRIPTION("PDM Template GPIO Driver");
+MODULE_DESCRIPTION("PDM Template PLATFORM Driver");
