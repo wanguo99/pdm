c5735b5c1f7f609eb653fadba51e7e3e68b906e9
wanguo <guohaoprc@163.com>
2025-01-13 16:16:08 +0800

    - Issues: update pdm_test - Date: 2025/01/13 16:16:08 - Author: wanguo

--- test/pdm_test.c
+++ test/pdm_test.c
@@ -4,65 +4,92 @@
 
 #include "pdm_test.h"
 
-void pdm_test_clear_screen() {
-    printf("\033[H\033[J");
-}
-
-static struct pdm_test_command_t commands[] = {
-    {"show_help", pdm_test_show_help},
-    {NULL, NULL}
+static struct pdm_test_unit *pdm_test_units[] = {
+    &pdm_test_unit_show_help,
+    NULL
 };
 
-int pdm_test_show_help(int argc, char *argv[])
+
+static struct pdm_test_unit *pdm_test_match_unit(struct pdm_test_unit **units,
+const char *name)
 {
-	struct pdm_test_command_t *cmd;
+    if (!units || !name) {
+        return NULL;
+    }
 
-	(void)argc;
-	(void)argv;
+    while (*units != NULL) {
+        if (strcmp((*units)->name, name) == 0) {
+            return *units;
+        }
+        units++;
+    }
 
-	printf("\n### Available commands:\n");
-	for (cmd = commands; cmd->name; cmd++) {
-		printf(" - %s\n", cmd->name);
-	}
-	printf("### \n\n");
-
-	return 0;
+    return NULL;
 }
 
-int pdm_test_dispatch_command(int argc, char *argv[])
+static int pdm_test_show_help(int argc, char *argv[])
 {
-	struct pdm_test_command_t *cmd;
-	if (!argv[0]) {
-		fprintf(stderr, "Error: No command provided.\n");
-		return -1;
-	}
+    struct pdm_test_unit **units = pdm_test_units;
 
-	for (cmd = commands; cmd->name; cmd++) {
-		if (!strcmp(cmd->name, argv[0])) {
-			printf("[CMD]: %s\n", cmd->name);
-			return cmd->func(argc, argv);
-		}
-	}
+    (void)argc;
+    (void)argv;
 
-	printf("Error: Unknown command '%s'. Showing help instead.\n", argv[0]);
-	return pdm_test_show_help(argc, argv);
+    printf("\n### Available commands:\n");
+
+    while (*units != NULL) {
+        printf("------------------\n");
+        printf(" - %s:\n", (*units)->name);
+        printf("   %s\n", (*units)->comment);
+        units++;
+    }
+
+    printf("### \n\n");
+
+    return 0;
 }
 
-void pdm_test_print_title() {
-	pdm_test_clear_screen();
-	printf("\n====== PDM Test ======\n");
+struct pdm_test_unit pdm_test_unit_show_help = {
+    .name = "show_help",
+    .comment = "show this message.",
+    .func = pdm_test_show_help
+};
+
+static int pdm_test_dispatch_command(int argc, char *argv[])
+{
+    struct pdm_test_unit *unit;
+
+    if (!argv[0]) {
+        fprintf(stderr, "Error: No command provided.\n");
+        return -1;
+    }
+
+    unit = pdm_test_match_unit(pdm_test_units, argv[0]);
+    if (!unit) {
+        printf("Error: Unknown command '%s'.\n", argv[0]);
+        return pdm_test_show_help(argc, argv);
+    }
+
+    printf("[CMD]: %s\n", unit->name);
+    return unit->func(argc, argv);
 }
 
-int main(int argc, char *argv[]) {
-	pdm_test_print_title();
+static void pdm_test_print_title()
+{
+    pdm_test_clear_screen();
+    printf("\n====== PDM Test ======\n");
+}
 
-	if (argc > 1 && !strncmp(argv[1], "./", 2)) {
-		argv[1] += 2;
-	}
+int main(int argc, char *argv[])
+{
+    pdm_test_print_title();
 
-	if (argc <= 1 || !strcmp(argv[1], "pdm_test")) {
-		return pdm_test_show_help(argc - 1, argv + 1);
-	} else {
-		return pdm_test_dispatch_command(argc - 1, argv + 1);
-	}
+    if (argc > 1 && !strncmp(argv[1], "./", 2)) {
+        argv[1] += 2;
+    }
+
+    if (argc <= 1 || !strcmp(argv[1], PDM_TEST_PROGRAM_NAME)) {
+        return pdm_test_show_help(argc - 1, argv + 1);
+    } else {
+        return pdm_test_dispatch_command(argc - 1, argv + 1);
+    }
 }
--- test/pdm_test.h
+++ test/pdm_test.h
@@ -1,13 +1,23 @@
 #ifndef _PDM_TEST_H_
 #define _PDM_TEST_H_
 
+#define PDM_TEST_PROGRAM_NAME	"pdm_test"
+
 typedef int (*command_func_t)(int argc, char *argv[]);
 
-struct pdm_test_command_t{
+struct pdm_test_unit{
 	const char *name;
+	const char *comment;
 	command_func_t func;
 };
 
-int pdm_test_show_help(int argc, char *argv[]);
+
+static inline void pdm_test_clear_screen(void)
+{
+	printf("\033[H\033[J");
+}
+
+struct pdm_test_unit pdm_test_unit_show_help;
+
 
 #endif /* _PDM_TEST_H_ */
